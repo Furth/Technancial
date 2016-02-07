695de5e66094aad08356d08aa79f1f2ba4296301
Raul Talledo <raul.talledo@soapros.pe>
2016-02-07 13:49:13 -0500

    modificaciones

--- Repository/IUnitOfWork.cs
+++ Repository/IUnitOfWork.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Repository
 {
-    public class Class1
+    public interface IUnitOfWork: IRepository
     {
+        int Save();
     }
 }
