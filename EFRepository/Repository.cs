622d3ef749544948c5469ed5e154bfa5dc85b623
Raul Talledo <raul.talledo@soapros.pe>
2016-02-07 13:43:43 -0500

    Repositorio implementador

---
+++ EFRepository/Repository.cs
@@ -0,0 +1,123 @@
+﻿using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using System.Data.Entity;
+
+namespace EFRepository
+{
+    public class Repository : IRepository
+    {
+        protected DbContext Context;
+
+        public Repository(DbContext context,
+                            bool autoDetectableChangesEnabled = false,
+                            bool proxyCreationEnabled = false)
+        {
+            this.Context = context;
+            this.Context.Configuration.AutoDetectChangesEnabled = autoDetectableChangesEnabled;
+            this.Context.Configuration.ProxyCreationEnabled = proxyCreationEnabled;
+        }
+
+        public TEntity Create<TEntity>(TEntity newEntity) where TEntity : class
+        {
+            TEntity Result;
+
+            try
+            {
+                Result = Context.Set<TEntity>().Add(newEntity);
+                TrySaveChanges();
+            }catch(Exception e)
+            {
+                throw (e);
+            }
+
+            return Result;
+        }
+
+        protected virtual int TrySaveChanges()
+        {
+            return Context.SaveChanges();
+        }
+
+        public bool Delete<TEntity>(TEntity deleteEntity) where TEntity : class
+        {
+            bool Result = false;
+
+            try
+            {
+                Context.Set<TEntity>().Attach(deleteEntity);
+                Context.Set<TEntity>().Remove(deleteEntity);
+                Result = TrySaveChanges() > 0;
+
+            }
+            catch (Exception e)
+            {
+                throw (e);
+            }
+
+            return Result;
+        }
+
+        public void Dispose()
+        {
+            if(Context != null)
+            {
+                Context.Dispose();
+            }
+        }
+
+        public TEntity FindEntity<TEntity>(Expression<Func<TEntity, bool>> criteria) where TEntity : class
+        {
+            TEntity Result = null;
+
+            try
+            {
+                Result = Context.Set<TEntity>().FirstOrDefault(criteria);
+            }catch(Exception e)
+            {
+                throw (e);
+            }
+
+            return Result;
+        }
+
+        public IEnumerable<TEntity> FindEntitySet<TEntity>(Expression<Func<TEntity, bool>> criteria) where TEntity : class
+        {
+            List<TEntity> Result = null;
+
+            try
+            {
+                Result = Context.Set<TEntity>().Where(criteria).ToList();
+             }
+            catch (Exception e)
+            {
+                throw (e);
+            }
+
+            return Result;
+        }
+
+        public bool Update<TEntity>(TEntity modifiedEntity) where TEntity : class
+        {
+            bool Result;
+
+            try
+            {
+                Context.Set<TEntity>().Attach(modifiedEntity);
+                Context.Entry<TEntity>(modifiedEntity).State = EntityState.Modified;
+                Result = TrySaveChanges() > 0;
+           
+            }
+            catch (Exception e)
+            {
+                throw (e);
+            }
+
+            return Result;
+        }
+    }
+}
---
+++ EFRepository/RepositoryUoW.cs
@@ -0,0 +1,39 @@
+﻿using Repository;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFRepository
+{
+    public class RepositoryUoW : Repository, IRepository, IUnitOfWork, IDisposable
+    {
+        public RepositoryUoW (DbContext context,
+                            bool autoDetectableChangesEnabled = false,
+                            bool proxyCreationEnabled = false):base(context, autoDetectableChangesEnabled, proxyCreationEnabled)
+        {
+        }
+
+        protected override int TrySaveChanges()
+        {
+            return 0;
+        }
+        int IUnitOfWork.Save()
+        {
+            int Result = 0;
+
+            try
+            {
+                Result = Context.SaveChanges();
+            }
+            catch(Exception e)
+            {
+                throw (e);
+            }
+
+            return Result;
+        }
+    }
+}
